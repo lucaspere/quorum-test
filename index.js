5f521b56547e0c8d174122caf88e45e8f840692b
lucaspere <lucasrupestre@hotmail.com>
2023-05-24 08:12:21 -0300

    test: create test to delivery one

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Initial");
+
+export const countLegSupporters = (dataset) => {
+  return {};
+};
---
+++ index.test.js
@@ -0,0 +1,59 @@
+import test from "ava";
+import { countLegSupporters } from "./index.js";
+
+const dataset = {
+  legislators: [
+    { id: "904789", name: "Rep. Don Bacon (R-NE-2)" },
+    { id: "1603850", name: "Rep. Jamaal Bowman (D-NY-16)" },
+  ],
+  vote_results: [
+    {
+      id: "92516784",
+      legislator_id: "904789",
+      vote_id: "3321166",
+      vote_type: "2",
+    },
+    {
+      id: "92516770",
+      legislator_id: "1603850",
+      vote_id: "3321166",
+      vote_type: "2",
+    },
+    {
+      id: "92279943",
+      legislator_id: "904789",
+      vote_id: "3314452",
+      vote_type: "2",
+    },
+    {
+      id: "92279920",
+      legislator_id: "1603850",
+      vote_id: "3314452",
+      vote_type: "1",
+    },
+  ],
+  votes: [
+    { id: "3314452", bill_id: "2900994" },
+    { id: "3321166", bill_id: "2952375" },
+  ],
+};
+
+test("should count legislator support", (t) => {
+  const result = countLegSupporters(dataset);
+  const expected = [
+    {
+      id: "904789",
+      name: "Rep. Don Bacon (R-NE-2)",
+      num_supported_bills: 0,
+      num_opposed_bills: 2,
+    },
+    {
+      id: "1603850",
+      name: "Rep. Jamaal Bowman (D-NY-16)",
+      num_supported_bills: 1,
+      num_opposed_bills: 1,
+    },
+  ];
+
+  t.deepEqual(result, expected);
+});
